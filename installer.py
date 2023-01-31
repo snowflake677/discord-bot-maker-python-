db5f4d3a61ece080c41874a17a1d8fa4c6ad0c91
snowflake677 <109577632+snowflake677@users.noreply.github.com>
2023-01-31 16:44:02 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,12 +1,11 @@
-import subprocess
-import sys
+import subprocess,sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 install('discord.py[voice]')
 install('requests')
 import requests
 def makefile(file=str):
-    f=open(r'dbmp\\'+file,'w')
+    f=open(file,'w')
     f=open(file,'w')
     f.write(requests.get("https://raw.githubusercontent.com/snowflake677/discord-bot-maker-python-/main/"+file).content.decode())
     f.close()
